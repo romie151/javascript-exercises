db2141e8578bdcb20c74d5909bf4599038d909cf
Romie Zelaya <romie151@yahoo.com>
2022-08-24 21:51:28 -0400

    adding activty 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,34 +1,37 @@
 const findTheOldest = function(arr) {
     let oldest = 0
-    let person = ''
+    let person = {}
     for (let i = 0; i < arr.length; i++) {
+        if (!arr[i].yearOfDeath) {
+            arr[i]["yearOfDeath"] = new Date().getFullYear()
+        }
         if (oldest < (arr[i].yearOfDeath - arr[i].yearOfBirth)) {
             oldest = arr[i].yearOfDeath - arr[i].yearOfBirth
-            person = arr[i].name
-        } 
-
+            person = arr[i]
+        }
     }
-    return(person)
     // console.log(person)
+    return(person)
+    
 };
 
 const people = [
     {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
-  findTheOldest(people)
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+]
+
+findTheOldest(people)
 // Do not edit below this line
 module.exports = findTheOldest;
