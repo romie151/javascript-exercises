a65465f23361083fd346c9f5df0eed324614a489
Romie Zelaya <romie151@yahoo.com>
2022-08-25 23:51:15 -0400

    finished the ceaser exercise

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,44 @@
-const caesar = function() {
+const caesar = function(str, num) {
+    let cypher = []
+    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let lower = "abcdefghijklmnopqrstuvwxyz"
 
+    if(num > 26) {
+        num = num % 26
+    }
+    for (let i = 0; i < str.length; i++) {
+        if ( !upper.includes(str[i]) && !lower.includes(str[i])) {
+            cypher.push(str[i])
+        } else if (upper.includes(str[i])){
+            for(let j = 0; j < upper.length; j++) {
+                if (str[i] === upper[j]) {
+                    if((j + num) >= 26) {
+                        cypher.push(upper[(j+num)% 26])
+                    } else if ((j + num) <= 0){
+                        cypher.push(upper[26 + (j + num)])
+                    } else {
+                        cypher.push(upper[j + num])
+                    }
+                }
+            }
+        } else if (lower.includes(str[i])) {
+            for(let j = 0; j < lower.length; j++) {
+                if (str[i] === lower[j]) {
+                    if((j + num) >= 26) {
+                        cypher.push(lower[(j+num)% 26])
+                    } else if ((j + num) <= 0){
+                        cypher.push(lower[26 + (j + num)])
+                    } else {
+                        cypher.push(lower[j + num])
+                    }
+                }
+            }
+        }
+    }
+    return cypher.join('')
+    // console.log(cypher.join(''))
 };
 
+caesar("Z", 1)
 // Do not edit below this line
 module.exports = caesar;
